48afecf7c2c664bfdc0b12e00dd59b73e4f38f03
Thomas Pickup <hello@thomaspickup.co.uk>
2019-01-07 11:19:41 +0000

    Section 1 Part 1 & 2 completed

---
+++ section1_code/section1_part1.R
@@ -0,0 +1,55 @@
+# Library Imports
+library(ggplot2)
+
+# Working Directory & Code Directory
+training_set.location <- "/Users/thomaspickup/Documents/University/CSC3060/Assignments/Assignment2_40145342/section1_code"
+section_code.location <- "/Users/thomaspickup/Documents/University/CSC3060/Assignments/Assignment2_40145342/section1_code"
+setwd(training_set.location)
+
+# Import features CSV
+ds <- read.table("40145342_features.csv", header=FALSE, sep=",")
+
+# Next we create the dataframe
+ds.df <- data.frame(ds)
+
+# We can now split the data into subsets based upon the label
+ds.lt_di <- subset(ds.df, V1 < 30)
+ds.lt <- subset(ds.df, V1 < 20)
+ds.di <- subset(ds.df, V1 > 20 & V1 < 30)
+ds.ma <- subset(ds.df, V1 > 30)
+
+# Now we add a column denoting whether something is a digit
+# If digit column is 1 then the row contains a digit else its a letter
+ds.lt_di$digit <- 0
+ds.lt_di$digit[ds.lt_di$V1 > 20] <- 1
+
+# Logistic Regression Model fit
+glm_fit <-glm(digit ~ V18, data = ds.lt_di, family = 'binomial') 
+
+# Works the predicted value based on the model created
+ds.lt_di[["predicted_val"]] = predict(glm_fit, ds.lt_di, type="response")
+
+# Zeroes out a predicted class field and then predicts based on a predicted value of 05
+ds.lt_di[["predicted_class"]] = 0
+ds.lt_di[["predicted_class"]][ds.lt_di[["predicted_val"]] > 0.5] = 1
+
+# Looks to see how many correct guesses there are
+correct_items = ds.lt_di[["predicted_class"]] == ds.lt_di[["digit"]] 
+
+# Works out the proportion correct
+proportion_correct <- nrow(ds.lt_di[correct_items,])/nrow(ds.lt_di)
+
+# Works out the proportion incorrect
+proportion_incorrect <- nrow(ds.lt_di[!correct_items,])/nrow(ds.lt_di)
+
+# Results table to plot
+proportion <- c("correct", "incorrect")
+values <- c(proportion_correct, proportion_incorrect)
+results <- data.frame(proportion, values)
+
+# Plot histogram of results
+ggplot(results, aes(x = proportion, y = values)) + geom_bar(position = 'dodge', stat="identity") + geom_text(aes(label=values), position=position_dodge(width=0.9), vjust=-0.25)
+
+# Change workspace to section_code and then saves plot
+setwd(section_code.location)
+ggsave("part1_accuracy_graph.png")
--- section3_code/section3_part1.R
+++ section3_code/section3_part1.R
@@ -17,35 +17,25 @@ for (i in 1:length(training_set.files)) {
 # Calls RBind on training_set.data and creates one dataframe from the individual ones
 training_set.dataframe <- do.call(rbind, training_set.data)
 
+model <- glm(V1 ~ V3 + V4 + V5,family=binomial(link='logit'),data=training_set.dataframe)
+?glm
 # Imports the classes needed for classification and plotting
-library(class)
-library(ggplot2)
+library(tree)
 
-# Makes a training set of indexes lower than 6
-training_set <- (training_set.dataframe$V2<6)
+training_set.tree <- tree(as.factor(V1)~., training_set.dataframe)
 
-# Gets the values of the rows of all the rows selected above for the training set
-training_set.x <- cbind(training_set.dataframe$V3,training_set.dataframe$V4,training_set.dataframe$V5,training_set.dataframe$V6,training_set.dataframe$V7,training_set.dataframe$V8,training_set.dataframe$V9,training_set.dataframe$V10,training_set.dataframe$V11,training_set.dataframe$V12)[training_set,]
+plot(training_set.tree)
+text(training_set.tree, pretty =0)
 
-# Stores the rows not selected for the training set in the test set
-test_set.x <- cbind(training_set.dataframe$V3,training_set.dataframe$V4,training_set.dataframe$V5,training_set.dataframe$V6,training_set.dataframe$V7,training_set.dataframe$V8,training_set.dataframe$V9,training_set.dataframe$V10,training_set.dataframe$V11,training_set.dataframe$V12)[!training_set,]
+training_set.train = sample(1: nrow(training_set.dataframe), 200)
+training_set.test = training_set.dataframe[-training_set.train,]
 
-# Gets the labels for the test and training sets
-training_set.labels <- training_set.dataframe$V1[training_set]
-test_set.labels <- training_set.dataframe$V1[!training_set]
+training_set.tree = tree(as.factor(V1)~.,training_set.dataframe, subset = training_set.train)
+training_set.pred = predict(training_set.tree, training_set.test, type = "class")
+training_set.test.labels <- training_set.dataframe$V1[-training_set.train]
 
-# Stores the values of K to test and creates a blank list for the accuracies
-ks = c(1,3,5,7,9,11,15,19,23,31)
-accuracies = c()
+mean(training_set.pred == training_set.test.labels)
 
-# Runs the KNN algorithm for all values of K above and stores the accuracy of each value of K
-for (k in ks){
-  knn.pred=knn(training_set.x,test_set.x,training_set.labels,k=k)
-  table(knn.pred,test_set.labels)
-  accuracies = cbind(accuracies, mean(knn.pred==test_set.labels))
-}
-
-# Plots as a bar chart
-accuracies.dataframe = as.data.frame(t(accuracies))
-cbind(accuracies.dataframe, ks)
-ggplot(accuracies.dataframe, aes(x=ks, y=V1)) + geom_bar(stat = "identity")
+training_set.tree.cv = cv.tree(training_set.tree, FUN = prune.misclass)
+plot(training_set.tree.cv)
+text(training_set.tree.cv, pretty =0)
